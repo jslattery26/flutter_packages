84a0d7089730cda650e5a4991288dd9fb053fcfe
Jack <jack@Mac-Pro.local>
2022-03-20 17:29:36 -0400

    remove weather

--- flutter_weather/lib/flutter_weather_bg.dart
+++
@@ -1,4 +0,0 @@
-library flutter_weather_bg_null_safety;
-
-export 'package:flutter_weather_bg_null_safety/utils/weather_type.dart';
-export 'package:flutter_weather_bg_null_safety/bg/weather_bg.dart' hide WeatherItemBg;
